430a1adadbcb396c6e535904f45d4115f60f7537
angelxdxdxdxd <143724173+angelxdxdxdxd@users.noreply.github.com>
2023-09-05 08:39:29 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables1;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaTareas {
+    
+    public static void main(String[] args) {
+        int hora;
+        String a = "desayuno";
+        String b = "me lavo los dientes";
+        String c = "tomo el camion";
+        String d = "llego  a la escuela";
+        String e = "tomo clase";
+        final int llegada;
+        hora = (int) 6.10;
+        llegada = 7;
+
+        System.out.println("En las mananas me deespierto a las " + hora + ", " + a + ", " + b + ", despues " + c + ", " + d + " a las " + llegada + " y " + e);
+    }
+    
+}
