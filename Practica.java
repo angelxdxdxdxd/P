fd7e34675802292aea6733870749eed4766b8644
angelxdxdxdxd <143724173+angelxdxdxdxd@users.noreply.github.com>
2023-09-07 08:45:17 -0600

    Add files via upload

---
+++ Practica.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newclass;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica {
+    
+    public static void main(String[] args) {
+        
+            int b = 70;
+            int h = 70;
+            
+            int areaCuadrado = b*h;
+            
+           var nombre = "Angel Manuel Colin Rodriguez";
+           
+            System.out.println("El area del cuadrado en etero es " + areaCuadrado + " y mi nombre es " + nombre);
+            
+            
+    }
+    
+}
